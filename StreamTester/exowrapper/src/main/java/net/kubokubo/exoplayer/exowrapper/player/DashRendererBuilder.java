978355964ea7fccf47cf785955539a62087b3a75
Jakub Ludma <jakub@zattoo.com>
2015-12-12 19:51:59 +0100

    initializing ExoPlayer object

---
+++ StreamTester/exowrapper/src/main/java/net/kubokubo/exoplayer/exowrapper/player/DashRendererBuilder.java
@@ -0,0 +1,8 @@
+package net.kubokubo.exoplayer.exowrapper.player;
+
+/**
+ * Created by kubo on 12/12/15.
+ */
+public class DashRendererBuilder {
+
+}
--- StreamTester/exowrapper/src/main/java/net/kubokubo/exoplayer/exowrapper/player/PlayerImpl.java
+++ StreamTester/exowrapper/src/main/java/net/kubokubo/exoplayer/exowrapper/player/PlayerImpl.java
@@ -1,9 +1,12 @@
 package net.kubokubo.exoplayer.exowrapper.player;
 
-import android.util.Log;
-import android.view.Surface;
+import android.net.Uri;
+import android.os.Handler;
+import android.view.SurfaceView;
 
-import java.net.URI;
+import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer.util.PlayerControl;
+
 import java.util.List;
 
 /**
@@ -12,10 +15,16 @@ import java.util.List;
 public class PlayerImpl implements Player {
 
     private static final String TAG = "PlayerImpl";
+    private static final int RENDERERS_COUNT = 2;
+
+    private ExoPlayer mExoPlayer;
+    private DashRendererBuilder mDashRendererBuilder;
+    private PlayerControl mPlayerControl;
+    private Handler mMainHandler;
 
     @Override
-    public void playUrl(Surface surface, URI streamUrl) {
-        Log.d(TAG, "WILL PLAY VIDEO");
+    public void init(SurfaceView surfaceView, Uri streamUri) {
+        mExoPlayer = ExoPlayer.Factory.newInstance(RENDERERS_COUNT);
     }
 
     @Override
@@ -24,7 +33,12 @@ public class PlayerImpl implements Player {
     }
 
     @Override
-    public void stop() {
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
 
     }
 
